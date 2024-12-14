09a7109d4903ef84258e1c823f36d4f3744671be
picker0198 <37229238+picker0198@users.noreply.github.com>
2024-12-14 10:56:29 +0500

    Update bleMidiHandler.dart
    added the new plus hardware

--- lib/bluetooth/bleMidiHandler.dart
+++ lib/bluetooth/bleMidiHandler.dart
@@ -18,7 +18,9 @@ typedef BluetoothErrorCallback = void Function(BleError, dynamic data);
 class BLEMidiHandler {
   //List of devices that doesn't advertise their midi service
   static const List<String> forcedDevices = [
-    "Chocolateplus" //Cuvave / M-Wave Chocolate plus
+     "FootCtrl", //Cuvave / M-Wave Chocolate
+     "FootCtrlPlus" //Cuvave / M-Wave Chocolate plus
+
   ];
 
   static final BLEMidiHandler _bleHandler = BLEMidiHandler._();
